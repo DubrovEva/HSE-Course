12a88f4a59448a33ec09c681e4b625adbdb17c5b
DubrovEva <42350479+DubrovEva@users.noreply.github.com>
2020-12-23 23:44:10 +0300

    Update biginteger.cpp

--- module-1/homework/BigInteger/biginteger.cpp
+++ module-1/homework/BigInteger/biginteger.cpp
@@ -29,59 +29,59 @@ BigInteger::BigInteger(std::string str) {
     improve();
 }
 
-BigInteger BigInteger::operator+(const BigInteger& other){
+BigInteger operator+(const BigInteger& first, const BigInteger& other){
     BigInteger result;
-    if (sign != other.sign) {
-        if (sign) {
+    if (first.sign != other.sign) {
+        if (first.sign) {
             BigInteger another = other;
             another.sign = true;
-            result = *this - another;
+            result = first - another;
         } else {
-            BigInteger another = *this;
+            BigInteger another = first;
             another.sign = true;
             result = another - other; 
             result.sign = !result.sign;
         }
     } else {
-        result.sign = sign;
+        result.sign = first.sign;
         bool carry = 0;
-        for (size_t i = 0; i < std::max(figures.size(),(other.figures).size()) || carry; ++i) {
+        for (size_t i = 0; i < std::max(first.figures.size(),(other.figures).size()) || carry; ++i) {
             if (i == result.figures.size())
                 result.figures.push_back(0);
-            int a = (i < figures.size() ? figures[i] : 0);
+            int a = (i < first.figures.size() ? first.figures[i] : 0);
             int b = (i < other.figures.size() ? other.figures[i] : 0);
             result.figures[i] =  a + carry + b;
-            carry = result.figures[i] >= base;
+            carry = result.figures[i] >= BigInteger::base;
             if (carry)  
-                result.figures[i] -= base;
+                result.figures[i] -= BigInteger::base;
         }   
     }
 
     result.improve();
     return result;
 }
-BigInteger BigInteger::operator-(const BigInteger& other){ 
+BigInteger operator-(const BigInteger& first, const BigInteger& other){ 
     BigInteger result;
-    if (*this == other)
+    if (first == other)
         result = BigInteger(0);
-    else if (sign && !other.sign) { // a - -b
+    else if (first.sign && !other.sign) { // a - -b
         BigInteger another = other;
         another.sign = true;
-        result = *this + another;
-    } else if (!sign && other.sign) { //-a - b
-        BigInteger another = *this;
+        result = first + another;
+    } else if (!first.sign && other.sign) { //-a - b
+        BigInteger another = first;
         another.sign = true;
         result = another + other;
         result.sign = false;
     } else {
-        if (*this > other)  {
+        if (first > other)  {
             result.sign = true;
-            result.figures = figures;
+            result.figures = first.figures;
             bool carry = 0;
             for (size_t i = 0; i < other.figures.size() || carry; ++i) {
                 result.figures[i] -= carry + (i < other.figures.size() ? other.figures[i] : 0);
                 carry = result.figures[i] < 0;
-                if (carry)  result.figures[i] += base;
+                if (carry)  result.figures[i] += BigInteger::base;
             }
             while (result.figures.size() > 1 && result.figures.back() == 0)
                 result.figures.pop_back();
@@ -89,10 +89,10 @@ BigInteger BigInteger::operator-(const BigInteger& other){
             result.sign = false;
             result.figures = other.figures;
             bool carry = 0;
-            for (size_t i = 0; i < figures.size() || carry; ++i) {
-                result.figures[i] -= carry + (i < figures.size() ? figures[i] : 0);
+            for (size_t i = 0; i < first.figures.size() || carry; ++i) {
+                result.figures[i] -= carry + (i < first.figures.size() ? first.figures[i] : 0);
                 carry = result.figures[i] < 0;
-                if (carry)  result.figures[i] += base;
+                if (carry)  result.figures[i] += BigInteger::base;
             }
             while (result.figures.size() > 1 && result.figures.back() == 0)
                 result.figures.pop_back();
@@ -102,16 +102,16 @@ BigInteger BigInteger::operator-(const BigInteger& other){
     result.improve();
     return result; 
 }   
-BigInteger BigInteger::operator*(const BigInteger& other){
+BigInteger operator*(const BigInteger& first, const BigInteger& other){
     BigInteger result;
-    result.sign = (sign && other.sign) || (!sign && !other.sign);
-    result.figures.resize(figures.size() + other.figures.size());
-    for (size_t i = 0; i < figures.size(); ++i)
+    result.sign = (first.sign && other.sign) || (!first.sign && !other.sign);
+    result.figures.resize(first.figures.size() + other.figures.size());
+    for (size_t i = 0; i < first.figures.size(); ++i)
         for (int j = 0, carry = 0; j < other.figures.size() || carry; ++j) {
             long long cur = 
-                result.figures[i + j] + figures[i] * 1ll * (j < (int)other.figures.size() ? other.figures[j] : 0) + carry;
-            result.figures[i + j] = int (cur % base);
-            carry = int (cur / base);
+                result.figures[i + j] + first.figures[i] * 1ll * (j < (int)other.figures.size() ? other.figures[j] : 0) + carry;
+            result.figures[i + j] = int (cur % BigInteger::base);
+            carry = int (cur / BigInteger::base);
         }
         
     result.improve();
@@ -133,25 +133,25 @@ BigInteger BigInteger::half(){
     return result; 
 }
 
-BigInteger BigInteger::operator/(const BigInteger& other){
+BigInteger operator/(const BigInteger& first, const BigInteger& other){
     bool carry = 0;
-    BigInteger l = 1, r = *this, diff = BigInteger(1);
+    BigInteger l = 1, r = first, diff = BigInteger(1);
 
     while (r - l > diff) {
         BigInteger m = (r + l).half();
-        if (m * other > *this)
+        if (m * other > first)
             r = m;
         else
             l = m;
     }
 
-    l.sign = (sign && other.sign) || (!sign && !other.sign);
+    l.sign = (first.sign && other.sign) || (!first.sign && !other.sign);
 
     l.improve();
     return l; 
 }
-BigInteger BigInteger::operator%(const BigInteger other) {
-    return *this - *this / other;
+BigInteger operator%(const BigInteger& first, const BigInteger other) {
+    return first - first / other;
 }
 
 BigInteger& BigInteger::operator+=(const BigInteger& other) {
@@ -202,40 +202,40 @@ BigInteger BigInteger::operator--(int) {
     return t;
 }
 
-bool BigInteger::operator==(const BigInteger& other) {
-    return (figures == other.figures) && (sign == other.sign);
+bool operator==(const BigInteger& first, const BigInteger& other) {
+    return (first.figures == other.figures) && (first.sign == other.sign);
 }
-bool BigInteger::operator!=(const BigInteger& other) {
-    return !(*this == other);
+bool operator!=(const BigInteger& first, const BigInteger& other) {
+    return !(first == other);
 }
-bool BigInteger::operator>(const BigInteger& other) {
-    if (*this == other)
+bool operator>(const BigInteger& first, const BigInteger& other) {
+    if (first == other)
         return false;
-    if (sign && !other.sign) 
+    if (first.sign && !other.sign) 
         return true;
-    else if (!sign && other.sign)
+    else if (!first.sign && other.sign)
         return false;
-    else if (sign && other.sign) {
-        if (figures.size() != other.figures.size()) {
-            return figures.size() > other.figures.size();
+    else if (first.sign && other.sign) {
+        if (first.figures.size() != other.figures.size()) {
+            return first.figures.size() > other.figures.size();
         } else {
-            for (int i = figures.size() - 1; i >= 0; --i) {
-                if (figures[i] != other.figures[i])
-                    return figures[i] > other.figures[i];
+            for (int i = first.figures.size() - 1; i >= 0; --i) {
+                if (first.figures[i] != other.figures[i])
+                    return first.figures[i] > other.figures[i];
             }
             return false;
         }
     }
     return false;
 }
-bool BigInteger::operator<(const BigInteger& other){
-    return !(*this > other) && (*this != other);
+bool operator<(const BigInteger& first, const BigInteger& other){
+    return !(first > other) && (first != other);
 }
-bool BigInteger::operator<=(const BigInteger& other){
-    return !(*this > other);
+bool operator<=(const BigInteger& first, const BigInteger& other){
+    return !(first > other);
 }
-bool BigInteger::operator>=(const BigInteger& other){
-    return !(*this < other);
+bool operator>=(const BigInteger& first, const BigInteger& other){
+    return !(first < other);
 }
 
 std::string BigInteger::toString() const {
